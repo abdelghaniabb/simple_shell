9e68986c657714d74db9faa01017e8f8743c9a68
boutalebhamza <boutaleb.hm@gmail.com>
2023-04-26 14:41:39 -0700

    task

--- main.h
+++ main.h
@@ -4,24 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
-#define MAX_LINE 1024
-
-
-
-extern char **environ;
-#define PROMPT "#cisfun$ "
-
-size_t replace_char(char *str, char old_char, char new_char);
-void execute_command(char *command, char *argv);
 
 #endif

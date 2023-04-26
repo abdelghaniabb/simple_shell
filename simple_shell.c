8286009104de08e35c8d0279fda36f24a1a4ccbe
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-26 15:22:17 +0100

    yyy

--- simple_shell.c
+++ simple_shell.c
@@ -6,39 +6,50 @@
 
 #define MAX_LINE 1024
 
-int main()
+int main(int argc, char *argv[])
 {
-    char line[MAX_LINE];
-    char *argv[MAX_LINE/2 + 1];
-    int should_run = 1;
-    pid_t pid;
-    int status;
+	char line[MAX_LINE];
+	char *arg[MAX_LINE/2 + 1];
+	int should_run = 1;
+	pid_t pid;
+	int status;
 
-    while (should_run) {
-        printf("#cisfun$ ");
-        fflush(stdout);
-        fgets(line, MAX_LINE, stdin);
-        if (line[strlen(line)-1] == '\n') {
-            line[strlen(line)-1] = '\0';
-        }
-        if (strcmp(line, "exit") == 0) {
-            should_run = 0;
-            break;
-        }
-        pid = fork();
-        if (pid < 0) {
-            perror("fork failed");
-            exit(1);
-        } else if (pid == 0) {
-            argv[0] = line;
-            argv[1] = NULL;
-            if (execve(argv[0], argv, NULL) == -1) {
-                perror("execve failed");
-                exit(1);
-            }
-        } else {
-            waitpid(pid, &status, 0);
-        }
-    }
-    return 0;
+	if (argc > 1)
+		printf("one arg");
+	while (should_run)
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
+		fgets(line, MAX_LINE, stdin);
+		if (line[strlen(line)-1] == '\n')
+		{
+			line[strlen(line)-1] = '\0';
+		}
+		if (strcmp(line, "exit") == 0)
+		{
+			should_run = 0;
+			break;
+		}
+		pid = fork();
+		if (pid < 0)
+		{
+			perror(argv[0]);
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			arg[0] = line;
+			arg[1] = NULL;
+			if (execve(arg[0], arg, NULL) == -1)
+			{
+				perror(argv[0]);
+				exit(1);
+			}
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+	}
+	return (0);
 }

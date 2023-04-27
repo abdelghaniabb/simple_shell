e009888504bcdc281430a4a686234947f6cd29dc
boutalebhamza <boutaleb.hm@gmail.com>
2023-04-26 17:59:09 -0700

    test

--- shell.c
+++ shell.c
@@ -1,5 +1,54 @@
 #include "main.h"
+/**
+ * _EOF - A function that chaecks if buffer is EOF
+ * @buffer: The pointer to the input string.
+ * Return: Nothing
+ */
+void _EOF(char *buffer)
+{
+	if (buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
 
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	free(buffer);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+  * execute_cmd - exeuc
+  * @buffer: bfr
+  */
+void execute_cmd(char *buffer, char *av)
+{
+	char *list[2];
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Error: "), free(buffer);
+		exit(8);
+	}
+	else if (pid == 0)
+	{
+		list[0] = buffer, list[1] = NULL;
+		if (execve(list[0], list, NULL) < -0)
+			perror(av), free(buffer), exit(8);
+	}
+	else
+	{
+		wait(&status);
+		if (status < 0)
+			free(buffer), exit(8);
+		free(buffer);
+	}
+
+}
 
 /**
   * main - entry point to shell
@@ -10,40 +59,22 @@
 
 int main(int __attribute__((unused)) argc, char *av[])
 {
-	char *buffer = NULL, *list[2];
-	int status;
+	char *buffer = NULL;
 	size_t buf_s = 0;
 	ssize_t len = 0;
-	pid_t pid;
 
 	while (1)
 	{
 		write(STDOUT_FILENO, "#cisfun$ ", 9);
 		len = getline(&buffer, &buf_s, stdin);
+		if (len == EOF)
+			_EOF(buffer);
 		if (*buffer == '\n')
 			free(buffer);
 		else
 		{
 			buffer[len - 1] = '\0';
-			pid = fork();
-			if (pid < 0)
-			{
-				perror("Error: "), free(buffer);
-				exit(8);
-			}
-			else if (pid == 0)
-			{
-				list[0] = buffer, list[1] = NULL;
-				if (execve(list[0], list, NULL) < -0)
-					perror(av[0]), free(buffer), exit(8);
-			}
-			else
-			{
-				wait(&status);
-				if (status < 0)
-					free(buffer), exit(8);
-				free(buffer);
-			}
+			execute_cmd(buffer, av[0]);
 			fflush(stdin), buffer = NULL, buf_s = 0;
 		}
 	}

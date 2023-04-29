34f216db3d05c537510a388c6d82bf821303f83d
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-29 15:03:54 +0100

    return

--- execute_cmd.c
+++ execute_cmd.c
@@ -6,60 +6,6 @@
   * @path: the path
   * Return: 0 - 1
   */
-
-/**
- * execute_cmd - execute a command
- * @tokens: the command and its arguments
- * @av: the name of the shell
- * @path: the path to the command
- * Return: the exit status of the command
- */
-int execute_cmd(char **tokens, char *av, char *path)
-{
-    pid_t pid;
-    int status;
-
-    pid = fork();
-    if (pid < 0)
-    {
-        perror(av);
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        if (execve(path, tokens, environ) == -1)
-        {
-            perror(av);
-            exit(2);
-        }
-    }
-    else
-    {
-        if (wait(&status) == -1)
-        {
-            perror("wait");
-            exit(2);
-        }
-
-        if (WIFEXITED(status))
-        {
-            return WEXITSTATUS(status);
-        }
-        else if (WIFSIGNALED(status))
-        {
-            fprintf(stderr, "%s: %s terminated by signal %d\n", av, tokens[0], WTERMSIG(status));
-            return 128 + WTERMSIG(status);
-        }
-        else
-        {
-            fprintf(stderr, "%s: %s exited abnormally\n", av, tokens[0]);
-            return 2;
-        }
-    }
-    return 0;
-}
-
-/*
 int execute_cmd(char **tokens, char *av, char *path)
 {
 	pid_t pid;
@@ -83,4 +29,3 @@ int execute_cmd(char **tokens, char *av, char *path)
 		wait(&status);
 	return (WEXITSTATUS(status));
 }
-*/

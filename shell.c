3bfe8fee29b59da0a157f848ad9e0e41eeba398c
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-29 15:28:21 +0100

    env2

--- shell.c
+++ shell.c
@@ -6,6 +6,16 @@
   * @av: arg value
   * Return: -1 | 1
   */
+
+#include "main.h"
+
+/**
+  * main - entry point to shell
+  * @argc: arg number
+  * @av: arg value
+  * Return: -1 | 1
+  */
+
 /**
  * set_env - set or update the value of an environment variable
  * @tokens: an array of command arguments
@@ -54,23 +64,68 @@ int unset_env(char **tokens)
 
 int main(int __attribute__((unused)) argc, char *av[])
 {
+    char *buffer = NULL, *path_env, *path, **tokens, st = 0;
+    size_t buf_s = 0;
+    int flag = 0, len = 0;
+
+    while (1)
+    {
+        if (strcmp(tokens[0], "setenv") == 0)
+        {
+            set_env(tokens);
+            continue;
+        }
+        else if (strcmp(tokens[0], "unsetenv") == 0)
+        {
+            unset_env(tokens);
+            continue;
+        }
+
+        if (isatty(0))
+            write(STDOUT_FILENO, "#meisfun$ ", 9);
+        len = getline(&buffer, &buf_s, stdin);
+        if (len == -1)
+            free(buffer), exit(0);
+        if (len == EOF)
+            _EOF(buffer);
+        if (*buffer == '\n')
+        {
+            free(buffer);
+            continue;
+        }
+        buffer[len - 1] = '\0';
+        if (is_whitespace(buffer) == 1)
+            continue;
+        tokens = make_tokens(buffer);
+        if (_strcmp(tokens[0], "exit") == 0)
+            free(buffer), free(tokens), exit(st);
+        if (_strcmp(tokens[0], "env") == 0)
+        {
+            free(tokens), print_env();
+            continue;
+        }
+        path_env = _getenv("PATH"), path = search_command(tokens[0], path, path_env);
+        if (path == NULL)
+            path = tokens[0];
+        else
+            flag = 1;
+        st = execute_cmd(tokens, av[0], path), free(buffer);
+        buffer = NULL, buf_s = 0, free(tokens);
+        if (flag == 1)
+            free(path);
+    }
+    return (0);
+}
+
+/*
+int main(int __attribute__((unused)) argc, char *av[])
+{
 	char *buffer = NULL, *path_env, *path, **tokens, st = 0;
 	size_t buf_s = 0;
 	int flag = 0, len = 0;
 
 	while (1)
 	{
-		if (strcmp(tokens[0], "setenv") == 0)
-{
-    set_env(tokens);
-    continue;
-}
-else if (strcmp(tokens[0], "unsetenv") == 0)
-{
-    unset_env(tokens);
-    continue;
-}
-
 		if (isatty(0))
 			write(STDOUT_FILENO, "#meisfun$ ", 9);
 		len = getline(&buffer, &buf_s, stdin);
@@ -106,3 +161,4 @@ else if (strcmp(tokens[0], "unsetenv") == 0)
 	}
 	return (0);
 }
+*/

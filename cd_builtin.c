1113ccdf7c14836e269faa30ef111cffafb0871c
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-05 15:50:54 +0100

    cd2

--- cd_builtin.c
+++ cd_builtin.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <limits.h>
 
+
 /**
   * cd_builtin - Change the current directory of the process.
   * @tokens: Command tokens (including the "cd" command and optional directory).
@@ -9,6 +10,7 @@
 int cd_builtin(char **tokens)
 {
     char *new_dir;
+    char cwd[PATH_MAX];
 
     if (tokens[1] == NULL || _strcmp(tokens[1], "~") == 0)
     {
@@ -16,24 +18,38 @@ int cd_builtin(char **tokens)
     }
     else if (_strcmp(tokens[1], "-") == 0)
     {
-        new_dir = _getenv("OLDPWD");
+        char *prev_dir = _getenv("OLDPWD");
+        if (prev_dir == NULL)
+        {
+            p_error(tokens[0], "No previous directory");
+            return 0;
+        }
+        new_dir = prev_dir;
     }
     else
     {
         new_dir = tokens[1];
     }
 
-    if (chdir(new_dir) == -1)
+    
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
     {
-        p_error(tokens[0], "Failed to change directory");
+        if (chdir(new_dir) == -1)
+        {
+            p_error(tokens[0], "Failed to change directory");
+        }
+        else
+        {
+            setenv("OLDPWD", cwd, 1);
+            if (getcwd(cwd, sizeof(cwd)) != NULL)
+            {
+                setenv("PWD", cwd, 1);
+            }
+        }
     }
     else
     {
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) != NULL)
-        {
-            setenv("PWD", cwd, 1);
-        }
+        p_error(tokens[0], "Failed to get current directory");
     }
 
     return 0;
